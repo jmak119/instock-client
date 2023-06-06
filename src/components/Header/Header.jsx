6d2c8dbdc162c3844acfa1214e85c54b42c86569
Shaymaa Abdou <shaymaaamin32@gmail.com>
2023-06-05 21:14:17 -0400

    add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import "./Header.scss";
+
+import { NavLink } from "react-router-dom/dist";
+import logo from "../../assets/logos/InStock-Logo_2x.png";
+
+function Header() {
+  const activeClassName = ({ isActive }) =>
+    !isActive ? "header__link" : "header__link header__link--active";
+
+  return (
+    <>
+      <section className="header">
+        <div className="page__section">
+          <Link to="/" className="header__logo-link">
+            <img className="header__logo" src={logo} alt="instock Logo" />
+          </Link>
+          <div className="header__links">
+            <NavLink to="/warehouse" className={activeClassName}>
+              Warehouse
+            </NavLink>
+            <NavLink to="/inventory" className={activeClassName}>
+              Inventory
+            </NavLink>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Header;
