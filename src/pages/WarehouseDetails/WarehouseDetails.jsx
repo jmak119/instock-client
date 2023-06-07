f7e661c85b6de4f6bd53248062002736e210951d
Ajay Singh <contact@ajaysingh.ca>
2023-06-07 15:56:08 -0400

    add warehouse header

--- src/pages/WarehouseDetails/WarehouseDetails.jsx
+++ src/pages/WarehouseDetails/WarehouseDetails.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import './WarehouseDetails.scss';
+import arrow from '../../assets/icons/arrow_back-24px.svg'
+import edit from '../../assets/icons/edit-24px.svg'
+
 export default function WarehouseDetails() {
   return (
-    <div>WarehouseDetails</div>
+    <div className='warehouse-details'>
+
+      <div className='warehouse-details__top'>
+            <div className='warehouse-details__top--name'>
+                <img src={arrow}/>
+                <h1>Washington</h1>
+            </div>
+            <img src={edit}/>
+      </div>
+
+    </div>
   )
 }
