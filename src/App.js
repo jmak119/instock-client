10a86c8b9e5e6dd29f47c9472eb0b6ce2008bc40
Ajay Singh <contact@ajaysingh.ca>
2023-06-07 10:31:22 -0400

    added most recent app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from './components/Header/Header';
 import Warehouse from './pages/Warehouse/Warehouse';
 import Inventory from './pages/Inventory/Inventory';
+import EditInventory from './pages/EditInventory/EditInventory';
+import Footer from './components/Footer/Footer';
+
 function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <Header /> */}
+        <Header />
         <Routes>
-          <Route path= "/" element={<Warehouse />} />
+          <Route path="/" element={<Warehouse />} />
           {/* <Route path= "/warehouse:id" element={<WarehouseDetails />} /> */}
-          <Route path= "/inventory" element={<Inventory/>} />
-          {/* <Route path= "/inventory/:id" element={<InventoryDetails/>} />
-          <Route path= "/Edit-Inventory/:id" element={<EditInventory/>} />
-          <Route path= "/Edit-Warehouse/:id" element={<EditWarehouse/>} /> */}
+          <Route path="/inventory" element={<Inventory />} />
+          {/* <Route path= "/inventory/:id" element={<InventoryDetails/>} /> */}
+          <Route path="/EditInventory/:id" element={<EditInventory />} />
+          {/* <Route path= "/Edit-Warehouse/:id" element={<EditWarehouse/>} /> */}
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
+
   );
 }
+
 export default App;
