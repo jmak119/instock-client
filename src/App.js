5958dd47ac889c41b07a82eb5888a26da91c0bdb
Shaymaa Abdou <shaymaaamin32@gmail.com>
2023-06-07 15:20:20 -0400

    add and edit  warehouse

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Header from './components/Header/Header';
-import Warehouse from './pages/Warehouse/Warehouse';
-import Inventory from './pages/Inventory/Inventory';
-import EditInventory from './pages/EditInventory/EditInventory';
-import Footer from './components/Footer/Footer';
+import Header from "./components/Header/Header";
+import Warehouse from "./pages/Warehouse/Warehouse";
+import Inventory from "./pages/Inventory/Inventory";
+import EditInventory from "./pages/EditInventory/EditInventory";
+import Footer from "./components/Footer/Footer";
+import EditWarehouse from "./pages/EditWarehouse/EditWarehouse";
+import AddWarehouse from "./pages/AddWarehouse/AddWarehouse";
+// import AddInventory from "./pages/AddInventory/AddInventory";
 
 function App() {
   return (
@@ -18,12 +21,13 @@ function App() {
           <Route path="/inventory" element={<Inventory />} />
           {/* <Route path= "/inventory/:id" element={<InventoryDetails/>} /> */}
           <Route path="/EditInventory/:id" element={<EditInventory />} />
-          {/* <Route path= "/Edit-Warehouse/:id" element={<EditWarehouse/>} /> */}
+          <Route path="/Edit-Warehouse/:id" element={<EditWarehouse />} />
+          {/* <Route path="/AddInventory/" element={<AddInventory />} /> */}
+          <Route path="/Add-Warehouse/" element={<AddWarehouse />} />
         </Routes>
         <Footer />
       </BrowserRouter>
     </>
-
   );
 }
 
--- src/pages/EditWarehouse/EditWarehouse.jsx
+++ src/pages/EditWarehouse/EditWarehouse.jsx
@@ -1,8 +1,91 @@
-import React from 'react'
-import './EditWarehouse.scss';
+import React from "react";
+import "./EditWarehouse.scss";
+import BackArrow from "../../assets/icons/arrow_back-24px.svg";
 
 export default function EditWarehouse() {
   return (
-    <div>EditWarehouse</div>
-  )
+    <div div className="edit-warehouse">
+      <div className="edit-warehouse__header">
+        <img
+          src={BackArrow}
+          alt="Back Arrow"
+          className="edit-warehouse__back-arrow"
+        />
+        <p className="edit-warehouse__title">EditWarehouse</p>
+      </div>
+      <div className="edit-warehouse__display">
+        <div>
+          <form className="edit-warehouse__form edit-warehouse__form--border">
+            <div className="edit-warehouse__details edit-warehouse__details--border">
+              <p className="edit-warehouse__details-title">Warehouse Details</p>
+              <label className="edit-warehouse__label">
+                Warehouse Name
+                <input
+                  type="text"
+                  className="edit-warehouse__input"
+                  placeholder="Washington"
+                />
+              </label>
+              <label className="edit-warehouse__label">
+                Street Address
+                <input
+                  type="text"
+                  className="edit-warehouse__input"
+                  placeholder="300 Pearl Street SW"
+                />
+              </label>
+              <label className="edit-warehouse__label">
+                City
+                <input type="text" className="edit-warehouse__input" />
+              </label>
+              <label className="edit-warehouse__label">
+                Country
+                <input type="text" className="edit-warehouse__input" />
+              </label>
+            </div>
+          </form>
+        </div>
+
+        <div>
+          <form className="edit-warehouse__form ">
+            <div className="edit-warehouse__details edit-warehouse__details--border">
+              <p className="edit-warehouse__details-title">Contact Details</p>
+              <label className="edit-warehouse__label">
+                Contact Name
+                <input
+                  type="text"
+                  className="edit-warehouse__input"
+                  placeholder="Washington"
+                />
+              </label>
+              <label className="edit-warehouse__label">
+                Position
+                <input
+                  type="text"
+                  className="edit-warehouse__input"
+                  placeholder="300 Pearl Street SW"
+                />
+              </label>
+              <label className="edit-warehouse__label">
+                Phone Number
+                <input type="text" className="edit-warehouse__input" />
+              </label>
+              <label className="edit-warehouse__label">
+                Email
+                <input type="text" className="edit-warehouse__input" />
+              </label>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="edit-inventory__footer">
+        <button className="edit-warehouse__button edit-warehouse__button--cancel">
+          Cancel
+        </button>
+        <button className="edit-warehouse__button edit-warehouse__button--save">
+          Save
+        </button>
+      </div>
+    </div>
+  );
 }
