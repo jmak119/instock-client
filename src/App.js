86adb83c9166ec841fbdb950d44b0b4b62023f94
Jonathan Mak <j.m4k119@gmail.com>
2023-06-07 10:16:11 -0400

    fix routes and conflicts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from './components/Header/Header';
 import Warehouse from './pages/Warehouse/Warehouse';
+import Inventory from './pages/Inventory/Inventory';
 import EditInventory from './pages/EditInventory/EditInventory';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <Header /> */}
+        <Header />
         <Routes>
-          <Route path= "/" element={<Warehouse />} />
+          <Route path="/" element={<Warehouse />} />
           {/* <Route path= "/warehouse:id" element={<WarehouseDetails />} /> */}
-          {/* <Route path= "/inventory" element={<Inventory/>} /> */}
+          <Route path="/inventory" element={<Inventory />} />
           {/* <Route path= "/inventory/:id" element={<InventoryDetails/>} /> */}
-          <Route path= "/EditInventory/:id" element={<EditInventory />} />
+          <Route path="/EditInventory/:id" element={<EditInventory />} />
           {/* <Route path= "/Edit-Warehouse/:id" element={<EditWarehouse/>} /> */}
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
 
