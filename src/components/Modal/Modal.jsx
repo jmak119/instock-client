1937995dd950fa17d541985f2f3fb1ab6be2826f
Ajay Singh <contact@ajaysingh.ca>
2023-06-07 23:07:44 -0400

    add delete functionality and ensure its working

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,22 +1,51 @@
-// Modal.js
-
-import React from 'react';
+import React, { useState } from 'react';
 import './Modal.scss';
-import closeicon from '../../assets/icons/close-24px.svg'
+import closeicon from '../../assets/icons/close-24px.svg';
+import axios from 'axios';
+import { apiUrl } from '../../utilities/api';
 
-const Modal = ({ isOpen, onClose }) => {
-  if (!isOpen) return false;
+const Modal = ({ isOpen, onClose, warehouseName, warehouseId }) => {
+
+  const handleDelete = () => {
+    axios
+      .delete(`${apiUrl}/api/warehouses/${warehouseId}`)
+      .then((response) => {
+        console.log('Warehouse deleted successfully');
+        onClose(); // Close the modal
+        window.location.reload(); // Refresh the page
+      })
+      .catch((error) => {
+        console.error('Error deleting warehouse:', error);
+        // Handle error if needed
+      });
+  };
+
+  if (!isOpen) return null;
 
   return (
     <div className='modal'>
       <div onClick={onClose} className='overlay'></div>
       <div className='modal-content'>
-        <h2>Delete Washington warehouse</h2>
-        <p>Please confirm that you'd like to delete the Washington warehouse from the list. This action cannot be undone.</p>
-        <img onClick={onClose} className='close-modal' src={closeicon} alt='Close' />
-        <div className='modal__buttons'>
-          <button onClick={onClose} className='modal__buttons--close'>Close</button>
-          <button className='modal__buttons--delete'>Delete</button>
+        <div className='modal-content__mobile'>
+          <div className='modal-content__text-close'>
+            <img onClick={onClose} className='close-modal' src={closeicon} alt='Close' />
+            <div className='modal-content__text-item'>
+              <div className='modal-content__text-item--text'>
+                <h2 className='modal-content__text-item--header'>Delete {warehouseName} warehouse</h2>
+                <p className='modal-content__text-item--paragraph'>
+                  Please confirm that you'd like to delete the {warehouseName} warehouse from the list. This action cannot be undone.
+                </p>
+              </div>
+              <div className='modal__buttons'>
+                <button onClick={onClose} className='modal__buttons--close'>
+                  Close
+                </button>
+                <button className='modal__buttons--delete' onClick={handleDelete}>
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
