d75131395d91d86b55849422b1a4d4ab68963ed5
Jesse Uszkay <jesseuszkay@Jims-MacBook-Pro.local>
2023-06-07 20:56:36 -0400

    Complete full page formatting

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import Header from './components/Header/Header';
-import Warehouse from './pages/Warehouse/Warehouse';
-import WarehouseDetails from './pages/WarehouseDetails/WarehouseDetails';
-import Inventory from './pages/Inventory/Inventory';
-import EditInventory from './pages/EditInventory/EditInventory';
-import Footer from './components/Footer/Footer';
+import Header from "./components/Header/Header";
+import Warehouse from "./pages/Warehouse/Warehouse";
+import WarehouseDetails from "./pages/WarehouseDetails/WarehouseDetails";
+import Inventory from "./pages/Inventory/Inventory";
+import EditInventory from "./pages/EditInventory/EditInventory";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Warehouse />} />
-          <Route path= "/warehouse/:id" element={<WarehouseDetails />} />
-          <Route path="/inventory" element={<Inventory />} />
-          {/* <Route path= "/inventory/:id" element={<InventoryDetails/>} /> */}
-          <Route path="/EditInventory/:id" element={<EditInventory />} />
-          {/* <Route path= "/Edit-Warehouse/:id" element={<EditWarehouse/>} /> */}
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <div className="app">
+        <div className="app__header">
+          <Header />
+        </div>
 
+        <div className="app__page">
+          <Routes>
+            <Route path="/" element={<Navigate to="/warehouse" />} />
+            <Route path="/warehouse" element={<Warehouse />} />
+            <Route path="/warehouse/:id" element={<WarehouseDetails />} />
+            <Route path="/inventory" element={<Inventory />} />
+            {/* <Route path= "/inventory/:id" element={<InventoryDetails/>} /> */}
+            <Route path="/inventory/:id" element={<EditInventory />} />
+            {/* <Route path= "/warehouse/:id" element={<EditWarehouse/>} /> */}
+          </Routes>
+        </div>
+
+        <div className="app__footer">
+          <Footer />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
