dec528bbaa4982a6141bd100169c21c66d027ddb
Jesse Uszkay <jesseuszkay@Jims-MBP.localdomain>
2023-06-09 16:29:01 -0400

    pre-weekend commit

--- src/pages/WarehouseDetails/WarehouseDetails.jsx
+++ src/pages/WarehouseDetails/WarehouseDetails.jsx
@@ -63,6 +63,8 @@ export default function WarehouseDetails() {
         />
       </div>
 
+      {/* Contact info section */}
+
       <div className="warehouse-details__middle">
         <div className="warehouse-details__middle--top">
           <p className="warehouse-details__middle-address">WAREHOUSE ADDRESS</p>
@@ -73,113 +75,96 @@ export default function WarehouseDetails() {
             {warehouseDetails.city}
           </p>
         </div>
-        {/* <div className="warehouse-details__middle--container"> */}
-        <div className="warehouse-details__middle--container--contact">
-          <p className="warehouse-details__middle-name">CONTACT NAME:</p>
-          <p className="warehouse-details__middle-name-item">
-            {warehouseDetails.contact_name}
-          </p>
-          <p className="warehouse-details__middle-name-item">
-            {warehouseDetails.contact_position}
-          </p>
+        <div className="warehouse-details__middle--container">
+          <div className="warehouse-details__middle--container--contact">
+            <p className="warehouse-details__middle-name">CONTACT NAME:</p>
+            <p className="warehouse-details__middle-name-item">
+              {warehouseDetails.contact_name}
+            </p>
+            <p className="warehouse-details__middle-name-item">
+              {warehouseDetails.contact_position}
+            </p>
+          </div>
+          <div className="warehouse-details__middle--container--contact">
+            <p className="warehouse-details__middle-name">
+              CONTACT INFORMATION:
+            </p>
+            <p className="warehouse-details__middle-name-item">
+              {warehouseDetails.contact_phone}
+            </p>
+            <p className="warehouse-details__middle-name-item">
+              {warehouseDetails.contact_email}
+            </p>
+          </div>
         </div>
-        <div className="warehouse-details__middle--container--contact">
-          <p className="warehouse-details__middle-name">CONTACT INFORMATION:</p>
-          <p className="warehouse-details__middle-name-item">
-            {warehouseDetails.contact_phone}
-          </p>
-          <p className="warehouse-details__middle-name-item">
-            {warehouseDetails.contact_email}
-          </p>
-        </div>
-        {/*  </div> */}
       </div>
-
-      <section className='inventory-list-top'>
-        <div className='inventory-list-top__head'>
-        <div className='inventory-list-top__row'>
-            <div className='inventory-list-top__item'>
-              <p className='inventory-list-top__text'>INVENTORY ITEM</p>
-              <img src={sort}/>
-            </div>
-
-            <div className='inventory-list-top__item'>
-              <p className='inventory-list-top__text'> CATEGORY</p>
-              <img src={sort}/>
-            </div>
-
-            <div className='inventory-list-top__item'>
-              <p className='inventory-list-top__text'>STATUS</p>
-              <img src={sort}/>
-            </div>
-        </div>
-
-        <div className='inventory-list-top__row'>
-            <div className='inventory-list-top__item'>
-              <p className='inventory-list-top__text'>QUANTITY</p>
-              <img src={sort}/>
-            </div>
-
-            <div className='inventory-list-top__item'>
-              <p className='inventory-list-top__text'>ACTIONS</p>
-              <img src={sort}/>
-            </div>
-
-            
-        </div>
-
-
-        </div>
-
-
-      </section>
 
       {/* Inventory Section */}
+
       {inventoryList.map((inventory) => {
         return (
-
-
           <div className="inventory-list__container" key={warehouseDetails.id}>
-          
-  <div className="inventory-list__text-box">
-    <div className="inventory-list__column">
-      <div className="inventory-list__content inventory-list__content--box">
-        <h3 className="inventory-list__mobile-header">INVENTORY ITEM</h3>
-        <div className="inventory-list__item-box">
-          <p className="inventory-list__item-name">{inventory.item_name}</p>
-          <img src={arrowside} alt="arrow icon" />
-        </div>
-      </div>
-      <div className="inventory-list__content inventory-list__content--long">
-        <h3 className="inventory-list__mobile-header">CATEGORY</h3>
-        <p className="inventory-list__item-name--category">{inventory.category}</p>
-      </div>
-    </div>
-    <div className="inventory-list__column">
-      <div className="inventory-list__content inventory-list__content--box">
-        <h3 className="inventory-list__mobile-header">STATUS</h3>
-        <p className={`inventory-list__status ${inventory.status === 'In Stock' ? 'inventory-list__status--in-stock' : 'inventory-list__status--out-of-stock'}`}>{inventory.status}</p>
-      </div>
-      <div className="inventory-list__content inventory-list__content--long">
-        <h3 className="inventory-list__mobile-header">QTY</h3>
-        <p className="inventory-list__item-name">{inventory.quantity}</p>
-      </div>
-    </div>
-  </div>
-  <div className="inventory-list__action">
-    <img src={DeleteButton} alt="delete icon" className="inventory-list__icon" />
-    <Link to={`/inventory/${id}/edit`} className="inventory-list__link">
-      <img src={EditButton} alt="edit icon" className="inventory-list__icon" />
-    </Link>
-  </div>
-</div>
-
-
-
-                        
+            <div className="inventory-list__text-box">
+              <div className="inventory-list__column">
+                <div className="inventory-list__content inventory-list__content--box">
+                  <h3 className="inventory-list__mobile-header">
+                    INVENTORY ITEM
+                  </h3>
+                  <div className="inventory-list__item-box">
+                    <p className="inventory-list__item-name">
+                      {inventory.item_name}
+                    </p>
+                    <img src={arrowside} alt="arrow icon" />
+                  </div>
+                </div>
+                <div className="inventory-list__content inventory-list__content--long">
+                  <h3 className="inventory-list__mobile-header">CATEGORY</h3>
+                  <p className="inventory-list__item-name--category">
+                    {inventory.category}
+                  </p>
+                </div>
+              </div>
+              <div className="inventory-list__column">
+                <div className="inventory-list__content inventory-list__content--box">
+                  <h3 className="inventory-list__mobile-header">STATUS</h3>
+                  <p
+                    className={`inventory-list__status ${
+                      inventory.status === "In Stock"
+                        ? "inventory-list__status--in-stock"
+                        : "inventory-list__status--out-of-stock"
+                    }`}
+                  >
+                    {inventory.status}
+                  </p>
+                </div>
+                <div className="inventory-list__content inventory-list__content--long">
+                  <h3 className="inventory-list__mobile-header">QTY</h3>
+                  <p className="inventory-list__item-name">
+                    {inventory.quantity}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="inventory-list__action">
+              <img
+                src={DeleteButton}
+                alt="delete icon"
+                className="inventory-list__icon"
+              />
+              <Link
+                to={`/inventory/${id}/edit`}
+                className="inventory-list__link"
+              >
+                <img
+                  src={EditButton}
+                  alt="edit icon"
+                  className="inventory-list__icon"
+                />
+              </Link>
+            </div>
+          </div>
         );
       })}
     </div>
-  
   );
 }
