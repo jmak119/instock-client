8dcc51eb3995c291ba3427d89d0d038374fce7b3
Ajay Singh <contact@ajaysingh.ca>
2023-06-09 15:11:01 -0400

    started inventory item details

--- src/pages/InventoryDetails/InventoryDetails.jsx
+++ src/pages/InventoryDetails/InventoryDetails.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import './InventoryDetails.scss';
+import Arrowback from "../../assets/icons/arrow_back-24px.svg";
+import EditButton from "../../assets/icons/edit-24px.svg";
+
 export default function InventoryDetails() {
   return (
-    <div>InventoryDetails</div>
+   <section className='inventory-details'>
+
+   <div className='inventory-details__header'>
+   <div className='inventory-details__header__long'>
+    <img src={Arrowback}/>
+    <p>Television</p>
+   </div>
+
+     <img src={EditButton} className='inventory-details__header--img'/>
+   </div>
+
+   </section>
   )
 }
